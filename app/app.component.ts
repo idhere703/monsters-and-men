3d9dd8f794ce25743cb70e68e6b59f7db02bd569
Brandon <brandon@gooddonegreat.com>
2016-06-30 22:28:54 -0400

    Next piece of this puzzle.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
-
-constructor(private heroService: HeroService) { }
-
 @Component({
   selector: 'my-app',
-  template:`
+  template: `
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
@@ -20,7 +17,7 @@ constructor(private heroService: HeroService) { }
     </ul>
     <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
-  styles:[`
+  styles: [`
     .selected {
       background-color: #CFD8DC !important;
       color: white;
@@ -70,13 +67,19 @@ constructor(private heroService: HeroService) { }
     }
   `],
   directives: [HeroDetailComponent],
-  heroes: Hero[],
   providers: [HeroService]
 })
-export class AppComponent {
-  title = 'Tour of Heroes';
-  heroes = HEROES;
-  selectedHero: Hero;
 
+export class AppComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+  constructor(private heroService: HeroService) { }
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit() {
+    this.getHeroes();
+  }
   onSelect(hero: Hero) { this.selectedHero = hero; }
 }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from "@angular/core";
-import { Heroes } from './mock-heroes';
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
   getHeroes() {
-    return HEROES;
+    return Promise.resolve(HEROES);
   }
 }
