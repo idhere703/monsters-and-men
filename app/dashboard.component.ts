a0f4435a1052a9687744384447921f0ab679ef6b
Brandon <brandon@gooddonegreat.com>
2016-07-05 21:20:43 -0400

    Added the hero details to the dashboard page.

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/component';
-
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-dashboard',
   templateUrl: 'app/dashboard.component.html'
 })
 
-export class DashboardComponent {
+export class DashboardComponent impements OnInit {
 
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
+  }
+
+  gotoDetail() { }
 }
